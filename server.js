bfb480404146b708712b462928e2be7e69b04d25
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-05-31 22:25:45 -0300

    First test

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 });
 
+fastify.get('/api/whoami', async (request, reply) => {
+  console.log(request.headers);
+  return { ipaddress: request.ip, language: request.headers['accept-language'], software: request.headers['user-agent'] }
+});
+
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(3000, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
